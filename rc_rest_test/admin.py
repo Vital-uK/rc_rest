fde2c3944ddfdd863685482b9375520387fbb52f
Vitaly Kolmykov <kolmykovvn@gmail.com>
2024-01-29 22:51:34 +0300

    add django_filters and basic models

--- rc_rest_test/admin.py
+++ rc_rest_test/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import rc_rest
 
 # Register your models here.
+
+class rc_rest_admin(admin.ModelAdmin):
+    list_display = ('id',  'title', 'description', 'date', 'author', 'comments')
+    list_display_links = ('id', 'title', 'comments')
+    search_fields = ('id', 'title', 'author')
--- rc_rest_test/models.py
+++ rc_rest_test/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class comment(models.Model):
+    text = models.TextField
+    author = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now_add=True)
+
+class comments(models.Model);
+
+class post(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blanc=True)
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.CharField(max_length=100, blanc=True)
+    comments = models.Model()
+    def __str__(self):
+        return self.title
