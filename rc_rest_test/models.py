2ea29ff789f500a44dabb237363cf285d7c4a755
Vitaly Kolmykov <kolmykovvn@gmail.com>
2024-01-30 01:12:14 +0300

    small fixes

--- rc_rest_test/models.py
+++ rc_rest_test/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class post(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, blank=True)
     description = models.TextField(default='')
     date = models.DateTimeField(auto_now_add=True)
     text = models.TextField(default='')
-    author = models.CharField(max_length=100)
+    author = models.CharField(max_length=100, blank=True)
     def __str__(self):
         return self.title
 
